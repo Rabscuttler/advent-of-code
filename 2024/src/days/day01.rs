86b4f7c4b176d740490b855b5e6138c3fc6b0a8d
Laurence Watson <laurence@furbnow.com>
2024-12-01 16:03:41 +0000

    best reddit solution

--- 2024/src/days/day01.rs
+++ 2024/src/days/day01.rs
@@ -76,3 +76,42 @@ mod tests {
         assert_eq!(part2(&test_input), 31);
     }
 }
+
+
+// Best solution on reddit:
+
+// use counter::Counter;
+
+// pub fn part_one(input: &str) -> Option<i32> {
+//     let (mut col1, mut col2): (Vec<i32>, Vec<i32>) = input
+//         .lines()
+//         .filter_map(|line| {
+//             let mut nums = line
+//                 .split_whitespace()
+//                 .filter_map(|x| x.parse::<i32>().ok());
+//             Some((nums.next()?, nums.next()?))})
+//         .unzip();
+  
+// col1.sort();
+// col2.sort();
+
+// let tot: i32 = col1.into_iter().zip(col2)
+//                    .map(|l| (l.0 - l.1).abs()).sum();
+
+// Some(tot)
+// }
+
+// pub fn part_two(input: &str) -> Option<usize> {
+//     let (col1, col2): (Counter<_>, Counter<_>) = input
+//         .lines()
+//         .filter_map(|line| {
+//             let mut nums = line
+//                 .split_whitespace()
+//                 .filter_map(|x| x.parse::<usize>().ok());
+//             Some((nums.next()?, nums.next()?))})
+//         .unzip();
+
+//     let a: usize = col1.into_iter()
+//            .map(|f| f.0 * f.1 * col2[&f.0]).sum();
+//     Some(a)
+// }
