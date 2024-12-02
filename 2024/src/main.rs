0cdfa82f39493081c597a11bf778a567cd1550d8
Laurence Watson <laurence@furbnow.com>
2024-12-02 21:40:26 +0000

    d2

--- 2024/src/main.rs
+++ 2024/src/main.rs
@@ -5,5 +5,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!();
     println!("\x1b[1;32mWelcome to Advent of Code 2024!\x1b[0m");
     days::day01::solve();
+    days::day02::solve();
     Ok(())
 }
